87647ccde679f721c52d0e094f0fb0cfaa3c6698
Martin <martingauna2000@gmail.com>
2022-07-01 11:31:47 -0300

    add carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 // import { CardList } from '../components/CardList/CardList'
 import ItemList from '../components/ItemList/ItemList'
+import Carousel from "../components/Carousel/Carousel";
 
 const Home = () => {
   return (
+    <>
+    <Carousel/>
+    <h2 className="card-text">Productos</h2>
     <ItemList />
+    </>
   )
 }
 
