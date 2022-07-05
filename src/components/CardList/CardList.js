5f2d78582be0a2ce7ed232f21f251f800eb5872c
Martin <martingauna2000@gmail.com>
2022-07-04 23:10:56 -0300

    readme

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect }from "react";
 import ItemList from "../ItemList/ItemList";
-import productsItems from "../../utils/productos";
 // import Item from "../Item/Item";
 // import ItemListContainer from "../ItemListContainer/ItemListContainer";
 
@@ -8,29 +7,7 @@ export const CardList = () => {
 
   const [products, setProducts] = useState([]);
 
-  
 
-  const getProducts = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(productsItems);
-      }, 1000);
-    });
-  };
-
-    useEffect( () => {
-        getProducts()
-        .then( (response) => {
-            // console.log("Then : Respuesta Promesa: ", response)
-            setProducts(response)
-        })
-        .catch( (err) => {
-            console.log("Catch: Fallo la llamada.", err)
-        })
-        .finally( () => {
-            console.log("Finally: termino la promesa")
-        })
-    }, [])
 
   return (
     <>
